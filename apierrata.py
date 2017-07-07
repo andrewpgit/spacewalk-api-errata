42a9140750d0e645dfe524b4dad65d3cfc5491b2
Andrew <andrii.pravdyvyi@gmail.com>
2017-07-07 20:01:01 +0300

    temp-branch

--- apierrata.py
+++ apierrata.py
@@ -3,7 +3,7 @@
 import xmlrpclib, time, getpass,sys,smtplib
 from datetime import datetime, timedelta
 from optparse import OptionParser
-
+#test branch
 URL = 'http://serverip or name/rpc/api'
 USERNAME = 'spaceadmin'
 PASSWORD = 'some password'
