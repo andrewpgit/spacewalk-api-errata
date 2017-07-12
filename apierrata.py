e5fa43b54cc0eccd81840576e5bedd8c52df696e
Andrew <andrii.pravdyvyi@gmail.com>
2017-07-12 09:09:40 +0300

    Options were added and Readme

--- apierrata.py
+++ apierrata.py
@@ -4,13 +4,6 @@ import xmlrpclib, time, getpass,sys,smtplib
 from datetime import datetime, timedelta
 from optparse import OptionParser
 
-URL = 'http://serverip or name/rpc/api'
-USERNAME = 'spaceadmin'
-PASSWORD = 'some password'
-client = xmlrpclib.Server(URL, verbose=0)
-session = client.auth.login(USERNAME, PASSWORD)
-
-
 
 def getChannelLabel():
  	getchannel = client.channel.listAllChannels(session)
@@ -40,21 +33,16 @@ def getLastDownloadPackages(DateTime):
 			 	yield lists
 
 
-def main(hours,minutes,channel_disable,flag_packages):
-	
+def main(options):
 	
 	hours, minutes = (options.hours,options.minutes)
 	DateTime = datetime.now() - timedelta(hours=hours,minutes=minutes)	
 	print DateTime
 	lineformat = '{0:<50s} {1:<10s} {2:<30s} {3:<10s}'
 	
+	#Chacked last errata
+	geterrata  = getErrata(DateTime,options.channel_disable)
 	
-
-#Chacked last errata
-	print channel_disable
-	geterrata  = getErrata(DateTime,channel_disable)
-	
-
 	for keys in geterrata:
 		#print keys['advisory'],keys['synopsis'],keys['advisory_type'],keys['issue_date']
 		name = keys['advisory']
@@ -70,8 +58,8 @@ def main(hours,minutes,channel_disable,flag_packages):
 			print '\n Description: {0:10s}\n'.format(errat['description'].encode('utf-8'))
 			print '-' * 120
 	
-#Get last packages were downloaded.
-	if not flag_packages:
+    #Get last packages were downloaded.
+	if not options.flag_packages:
 		getlist = getLastDownloadPackages(DateTime)
 		print lineformat.format('Package Name','Version','Release', 'Last_modified')
 		print lineformat.format('------------','-------','-------', '--------------')
@@ -82,7 +70,7 @@ def main(hours,minutes,channel_disable,flag_packages):
 
 if __name__ == "__main__":
 
-	usage = "usage %prog[option]"
+	usage = "usage %prog [option]"
 	parser = OptionParser(usage=usage)
 	parser.add_option("--disable", dest="channel_disable",default='None',
 		help="Do not see errara for channel label.[ centos7|epel7 ]")
@@ -92,6 +80,20 @@ if __name__ == "__main__":
 		help="Set period of time in hours",default='24')
 	parser.add_option('--minutes',type='int',dest='minutes',
 		help="Set period of time in minutes", default='00')
+	parser.add_option('-p','--password',dest='password',default='None',
+		help='Set user password')
+	parser.add_option('-u', '--user', dest='username',default='None',
+		help='Set user name')
+
 	options, args = parser.parse_args()
+
+	if not (options.username and options.password):
+		parser.print_help()
+		parser.error("Must provide a username and password to content to spacewalk api")
+	else:
 	
-	main(options.hours,options.minutes,options.channel_disable,options.flag_packages)
+		URL = 'http://localhost/rpc/api'
+		client = xmlrpclib.Server(URL, verbose=0)
+		session = client.auth.login(options.username,options.password)
+
+		main(options)
